c145665e948e1d28217bd5691404e6ad5e44a4cf
Joakim Bakke Hellum <joakim.hellum@protonmail.ch>
2026-03-30 22:38:09 +0200

    remove overly specific references

--- examples/functions/derive_key/function.tf
+++ examples/functions/derive_key/function.tf
@@ -1,17 +1,15 @@
-# Derive an Ed25519 private key from an enrollment key using HKDF-SHA256.
-# The resulting PEM key can be used with tls_self_signed_cert to create
-# a CA certificate that matches pigeon-enroll's DeriveNamedCA output.
+# Derive an Ed25519 private key using HKDF-SHA256
 output "private_key_pem" {
-  value     = provider::hkdf::derive_key(var.enrollment_key_base64, "pigeon-enroll ca nomad key v1", "ed25519")
+  value     = provider::hkdf::derive_key(var.secret, "my-app ca key v1", "ed25519")
   sensitive = true
 }
 
 # Use with tls_self_signed_cert to create a deterministic CA certificate
-resource "tls_self_signed_cert" "nomad_ca" {
-  private_key_pem = provider::hkdf::derive_key(var.enrollment_key_base64, "pigeon-enroll ca nomad key v1", "ed25519")
+resource "tls_self_signed_cert" "ca" {
+  private_key_pem = provider::hkdf::derive_key(var.secret, "my-app ca key v1", "ed25519")
 
   subject {
-    common_name = "nomad"
+    common_name = "my-app"
   }
 
   validity_period_hours = 876000
